612a7f8f084fa47656830983db55b0391967675a
Osvaldo Rodrigues De Faria Junior <osvaldo.junior2@estudante.ufla.br>
2022-06-25 18:27:09 -0300

    Conta Bancaria

---
+++ projetosP1/blueJ/contaBancaria/Banco.java
@@ -0,0 +1,108 @@
+import java.util.Scanner;
+/**
+ * Classe Banco, classe para interagir com a classe Conta.
+ * 
+ * Autor Osvaldo 
+ * Versão 25-06-2022
+ */
+
+import javax.lang.model.util.ElementScanner14;
+
+public class Banco {
+    private Conta conta;
+    Scanner entrada = new Scanner(System.in);
+        
+    int opcao = 0;
+    while(opcao != 5)
+    {
+        opcao = exibirMenu(entrada);
+        if(opcao == 1)
+        {
+            System.out.println("Deseja criar uma conta com saldo e limite com um valor  inicial digite 1, ou digite 2 para criar uma conta com saldo e limite igual a zero : ");
+            int n = entrada.nextInt();
+            if(n == 1)
+            {
+                System.out.println(" digite o saldo depois limite : ");//melhorar este texto
+                double saldo = entrada.nextDouble();
+                double limite = entrada.nextDouble();
+                conta = new Conta(saldo,limite);
+            }
+            else if(n == 2)
+            {
+                conta = new Conta();
+            }
+            else 
+            {
+                System.out.println(" Opção Inválida! ");
+            }
+        }
+        else if(opcao == 2)
+        {
+            System.out.println("O saldo é de : " + conta.getSaldo());
+        }
+        
+        else if(opcao == 3)
+        {
+            System.out.println("Insira o valor a ser depositado : ");
+            double quanto = entrada.nextDouble();
+            conta.deposito(quanto);
+            System.out.println("Depósito realizado! ");
+        }
+        
+        else if(opcao == 4)
+        {
+            System.out.println("Digite o valor a ser sacado : ");
+            double quanto = entrada.nextDouble();
+            if(conta.saque(quanto))
+            {
+                System.out.println("Saque realizado! ");
+            }
+            else
+            {
+                System.out.println("Saldo ou limite insuficiente! ");
+            }
+        }
+        
+        else if(opcao == 5)
+        {
+            System.out.println("Ate mais! ");
+        }
+        
+        else
+        {
+            System.out.println("Opçao invalida! ");
+        }
+        
+        if(opcao !=5 )
+        {
+            aguardarEnter(entrada);
+        }
+    }
+
+       
+    /**
+     * Exibe o menu 
+     */
+    private static int exibirMenu(Scanner entrada)
+    {
+        System.out.println("");
+        System.out.println("1) Criar conta ");
+        System.out.println("2) consultar saldo ");
+        System.out.println("3) Depositar ");
+        System.out.println("4) Reakizar saque ");
+        System.out.println("5) Sair");
+        System.out.println("Digite sua opçao desejada : ");
+        int opcaoDoUsuario = Integer.parseInt(entrada.nextLine());
+        return opcaoDoUsuario;
+    }
+        
+    /**
+     * Aguarda o usuario digitar o enter pra exibir o menu de novo.
+     */
+    private static void aguardarEnter(Scanner entrada)
+    {
+        System.out.println("Digite ENTER para continuar ");
+        System.out.println("");
+        entrada.nextLine();
+    }
+}
--- projetosP1/contaBancaria/src/Banco.java
+++ projetosP1/contaBancaria/src/Banco.java
@@ -22,7 +22,7 @@ public class Banco {
                 int n = entrada.nextInt();
                 if(n == 1)
                 {
-                    System.out.println(" digite o saldo depois limite : ");
+                    System.out.println(" digite o saldo depois limite : ");//melhorar este texto
                     double saldo = entrada.nextDouble();
                     double limite = entrada.nextDouble();
                     conta = new Conta(saldo,limite);
@@ -36,31 +36,30 @@ public class Banco {
                     System.out.println(" Opção Inválida! ");
                 }
             }
-            //mudei até aqui !
             else if(opcao == 2)
             {
-                System.out.println("Carro de nome : " + carro.getNome() + " e velocidade " + carro.getVelocidade());
+                System.out.println("O saldo é de : " + conta.getSaldo());
             }
             
             else if(opcao == 3)
             {
-                System.out.println("Quanto voce quer acelerar : ");
-                int quanto = Integer.parseInt(entrada.nextLine());
-                carro.acelerar(quanto);
-                System.out.println("Nova velocidade e " + carro.getVelocidade());
+                System.out.println("Insira o valor a ser depositado : ");
+                double quanto = entrada.nextDouble();
+                conta.deposito(quanto);
+                System.out.println("Depósito realizado! ");
             }
             
             else if(opcao == 4)
             {
-                System.out.println("Quanto voce quer reduzir : ");
-                int quanto = Integer.parseInt(entrada.nextLine());
-                if(carro.reduzir(quanto))
+                System.out.println("Digite o valor a ser sacado : ");
+                double quanto = entrada.nextDouble();
+                if(conta.saque(quanto))
                 {
-                    System.out.println("Nova velocidade e " + carro.getVelocidade());
+                    System.out.println("Saque realizado! ");
                 }
                 else
                 {
-                    System.out.println("Reduzida maior ou igual a velocidade do carro, carro parado! ");
+                    System.out.println("Saldo ou limite insuficiente! ");
                 }
             }
             
@@ -87,10 +86,10 @@ public class Banco {
     private static int exibirMenu(Scanner entrada)
     {
         System.out.println("");
-        System.out.println("1) Criar o carro ");
-        System.out.println("2) Exibir os dados do carrro ");
-        System.out.println("3) Acelerar o carro ");
-        System.out.println("4) Reduzir a velocidade do carro ");
+        System.out.println("1) Criar conta ");
+        System.out.println("2) consultar saldo ");
+        System.out.println("3) Depositar ");
+        System.out.println("4) Reakizar saque ");
         System.out.println("5) Sair");
         System.out.println("Digite sua opçao desejada : ");
         int opcaoDoUsuario = Integer.parseInt(entrada.nextLine());
@@ -106,4 +105,4 @@ public class Banco {
         System.out.println("");
         entrada.nextLine();
     }
-}
+
